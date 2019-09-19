0b084ab8e1d0a9be32004c9204fd4661fe423981
Saeid Amanzadeh <saeid.amanzadehgharehtappeh@pearson.com>
2019-09-19 00:07:35 -0500

    added imageList 2

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const ImageList = props => {
-    const images = props.images.map(image => {
-        return <img src={image.urls.regular} />;
+    const images = props.images.map(({id, description, urls}) => {
+        return <img key={id} alt={description} src={urls.regular} />;
     });
 
     return <div>{images}</div>;
